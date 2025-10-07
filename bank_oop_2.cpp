e8733c10d8ff66009c351b70c90b95ebb1295f81
AbdullahSaied52 <a9407602@gmail.com>
2025-10-07 20:43:48 +0300

    sending segment by segment

---
+++ bank_oop_2.cpp
@@ -0,0 +1,14 @@
+#include"cls_login.h"
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        cls_login::login();
+        if (stop )
+            break;
+    }
+
+    return 0;
+}
